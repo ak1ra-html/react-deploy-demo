a30eb79465ab46ab252e62ca3cb4b446a0231f5d
ak1ra-html <akiralonely404@gmail.com>
2026-06-11 21:22:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      textAlign: 'center',
+      marginTop: '100px',
+      fontFamily: 'Arial'
+    }}>
+      <h1>Deployment React Berhasil 🚀</h1>
+      <p>Aplikasi berhasil dijalankan dan siap deploy.</p>
     </div>
   );
 }
